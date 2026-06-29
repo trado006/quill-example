3534d93895ff04c062c844cc83f1755906b23a08
tradokim <tra.do.kim@beetechjsc.com>
2026-06-29 14:55:18 +0700

    update: view for demo app

--- packages/demo/routes/index.js
+++ packages/demo/routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index.jade', { title: 'Express' });
-  // res.render('index.ejs', { name: 'Express' });
-  res.render('index.njk', {
-    name: 'Tra'
-  });
+  res.send('Hello World');
 });
+
+const userRoute = require("./users.route");
+const testRoute = require("./test.route");
+
+router.use("/users", userRoute);
+router.use("/test", testRoute);
 
 module.exports = router;
---
+++ packages/demo/routes/test.route.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/jade', function(req, res, next) {
+  res.render("test/example.jade",{
+        title:"Test Jade",
+        message:"Hello Jade Template",
+        users:[
+            {name:"John"},
+            {name:"Mary"}
+        ]
+    });
+});
+
+router.get('/ejs', function(req, res, next) {
+  res.render('test/example.ejs', { name: 'EJS engine' });
+});
+
+router.get('/njk', function(req, res, next) {
+  res.render('test/example.njk', { name: 'NJK engine' });
+});
+
+router.get('/quill', function(req, res, next) {
+  res.render('test/quill.ejs', { name: 'EJS engine' });
+});
+
+
+module.exports = router;
